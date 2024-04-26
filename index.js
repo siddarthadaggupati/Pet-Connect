7df72b5b7f8fcf31a7f1cd07f266c35d274b37ff
siddarthadaggupati <115910778+siddarthadaggupati@users.noreply.github.com>
2024-04-27 01:47:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cors(
     {
-        origin : ["https://client-omega-lyart.vercel.app"],
+        origin : "*",
         methods : ["POST","GET"],
         credentials : true
     }
